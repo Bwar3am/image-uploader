1c0b429a0cd61af9b5e7376f7fa118c2f0987cee
amoobarsam <141170967+amoobarsam@users.noreply.github.com>
2024-06-30 19:17:22 +0330

    config
    config

--- imageuploader/urls.py
+++ imageuploader/urls.py
@@ -1,15 +1,16 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
-from myapp import *
+from myapp import views
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("" , views.home)
+    path("" , views.home )
     
-]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
 
-#print(static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT))
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .forms import  *
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request , "myapp/home.html")
+    if request.method == "POST":
+        form = imageform(request.POST , request.FILES)
+        if form.is_valid():
+            form.save()
+    form = imageform()
+    img = image.objects.all()
+    return render(request , "myapp/home.html" , {"img":img, "form":form})
